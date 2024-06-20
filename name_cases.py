c51a592204105478c17167601a4d2b8cdb107ca4
linzhenghao233 <1392872316@qq.com>
2024-06-21 01:32:46 +0800

    0621

---
+++ name_cases.py
@@ -0,0 +1,28 @@
+name = "Lin Zhenghao"
+print(f"Hello {name}, would you like to learn some Python today?\n")
+print(name.upper())
+name = name.lower()
+print(name)
+print(name.title())
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+
+famous_person = "Albert Einstein"
+message = "A person who never made a mistake never tried anything new."
+print(f'{famous_person} once said, "{message}"')
+
+name = "\tlinzhenghao\t\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
+
+print(4 + 4)
+print(16 - 8)
+print(2 * 2 * 2)
+print(16 / 2)
+
+favorite_num = 2
+print(f"My favorite number is {favorite_num}")
